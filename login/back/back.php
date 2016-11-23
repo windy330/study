33fa24242df42adf9ba2f60b9e02b3b8fb28e64f
weixiaodong <2758570254@qq.com>
2016-11-23 22:28:40 +0800

    项目阶段性提交

--- login/back/back.php
+++ login/back/back.php
@@ -3,7 +3,8 @@ header('Content-Type: application/json; charset=utf-8');
 $result = array(
 	'code'=> 10000,
 	'msg'=>'OK',
-	'result'=> 'http://www.baidu.com'
+	'result'=> 'http://www.study.com/login/home.html'
 	);
 echo json_encode($result);
+sleep(6);
 ?>
