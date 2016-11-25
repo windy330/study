ce391199e052505ad518b53125e71e65cec663b1
weixiaodong <2758570254@qq.com>
2016-11-26 07:27:29 +0800

    项目阶段性提交

--- login/back/back.php
+++ login/back/back.php
@@ -1,10 +1,14 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
-$result = array(
+//$date = $_POST();
+//print_r($date);
+	$result = array(
 	'code'=> 10000,
 	'msg'=>'OK',
 	'result'=> 'http://www.study.com/login/home.html'
 	);
-echo json_encode($result);
+json_encode($result);
 sleep(6);
+}
+
 ?>
